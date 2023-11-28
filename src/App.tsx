71f0db608148605ca9cb87949f169f12bdcac8e3
tushar.kawade <tushar.kawade@encora.com>
2023-11-28 11:26:03 +0530

    material ui installation testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import "./App.css";
+import { Button, ButtonGroup } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
+      <ButtonGroup
+        variant="contained"
+        aria-label="outlined primary button group"
+      >
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup>
       <h1>Hello world</h1>
     </div>
   );
