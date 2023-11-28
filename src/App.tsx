d742f02f20cdf2555315dccc0f90a3b9f700a9d3
tushar.kawade <tushar.kawade@encora.com>
2023-11-28 11:33:38 +0530

    login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import React from "react";
 import "./App.css";
 import { Button, ButtonGroup } from "@mui/material";
+import Login from "./Auth/Login";
 
 function App() {
   return (
     <div className="App">
-      <ButtonGroup
-        variant="contained"
-        aria-label="outlined primary button group"
-      >
-        <Button>One</Button>
-        <Button>Two</Button>
-        <Button>Three</Button>
-      </ButtonGroup>
-      <h1>Hello world</h1>
+      <Login />
     </div>
   );
 }
