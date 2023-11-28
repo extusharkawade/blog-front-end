3f0edd29122cbeaadefa561bc40b76393b1f9144
tushar.kawade <tushar.kawade@encora.com>
2023-11-28 11:06:30 +0530

    removing changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey Mayuri..!! you're an idiot</h1>
+      <h1>Hello world</h1>
     </div>
   );
 }
